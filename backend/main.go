d2eb1283bc6588073aa147115be17d7c790f5b4d
abraao <abraao-284@outlook.com>
2025-10-29 20:24:42 -0300

    feat(main) merged backend

--- backend/main.go
+++ backend/main.go
@@ -1,24 +1,16 @@
 package main
 
 import (
-	"encoding/json"
-	"meu_ip/middleware"
+	"fmt"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
-type Message struct {
-	Content string `json:"content"`
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	response := Message{Content: r.RemoteAddr}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK) // optional but good practice
-	json.NewEncoder(w).Encode(response)
-}
-
 func main() {
-	http.HandleFunc("/", helloHandler)
-	handler := middleware.EnableCORS(http.DefaultServeMux)
-	http.ListenAndServe(":8080", handler)
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, r.RemoteAddr)
+	}).Methods("GET")
+
+	http.ListenAndServe(":8080", r)
 }
