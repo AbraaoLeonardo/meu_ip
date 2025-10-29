3bce839f4ab2358164192e3c0f00b99391b0ae39
abraao <abraao-284@outlook.com>
2025-10-29 19:58:56 -0300

    feat(frontend) - iniciando o front

---
+++ frontend/src/App.vue
@@ -0,0 +1,34 @@
+<script setup></script>
+
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+    <button @click="fetchMessage">Obter Mensagem</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: ''
+    };
+  },
+  methods: {
+    async fetchMessage() {
+      try {
+        const response = await axios.get('http://localhost:8080/');
+        this.message = response.data.content;
+      } catch (error) {
+        console.error('Erro ao buscar a mensagem:', error);
+      }
+    }
+  }
+}
+</script>
+
+
+<style scoped></style>
